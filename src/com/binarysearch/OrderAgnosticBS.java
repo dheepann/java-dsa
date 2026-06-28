8ef2e611cf534502a9f287435d3f27163f8fd1ad
Dheepan <dheeepans09@gmail.com>
2026-06-28 19:41:58 +0530

    added order agnostic binary search algorithm

--- src/com/binarysearch/OrderAgnosticBS.java
+++ src/com/binarysearch/OrderAgnosticBS.java
@@ -1,25 +1,25 @@
 package com.binarysearch;
 
-public class BinarySearch {
+public class OrderAgnosticBS {
     public static void main(String[] args) {
-        int[] arr = {-8, -3, 0, 18, 24, 66, 79, 100};
-        int target = 91;
-        int ans = binarySearch(arr, target);
+        int[] arr = {100, 79, 66, 24, 6, 0, -3, -8};
+        int target = 66;
+        int ans = orderagnosticbs(arr, target);
         System.out.println(ans);
     }
 
     // return the index
     // return -1 if it does not exist
-    static int binarySearch(int[] arr, int target) {
+    static int orderagnosticbs(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
             // find the middle element
             int mid = start + (end - start) / 2; // might be possible that (start + end) exceeds the range of int in java
             if (target > arr[mid]) {
-                start = mid + 1;
-            } else if (target < arr[mid]) {
                 end = mid - 1;
+            } else if (target < arr[mid]) {
+                start = mid + 1;
             } else {
                 return mid;
             }
