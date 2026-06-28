14ea9c8679a8329a10c86629fbd971618bbd7b4d
Dheepan <dheeepans09@gmail.com>
2026-06-29 00:40:12 +0530

    renamed binarysearch() to ceiling()

--- src/com/binarysearch/Floor.java
+++ src/com/binarysearch/Floor.java
@@ -1,37 +1,29 @@
 package com.binarysearch;
 
-public class Ceiling {
+public class Floor {
     public static void main(String[] args) {
         int[] arr = {-8, -3, 0, 18, 24, 66, 79, 100};
-        int target = -4;
-        int ans = ceiling(arr, target);
+        int target = -3;
+        int ans = binarySearch(arr, target);
         System.out.println(ans);
     }
 
     // return the index
     // return -1 if it does not exist
-    static int ceiling(int[] arr, int target) {
-
-        // but what if the target is greater than the greatest number in the array
-        if (target > arr[arr.length - 1]) {
-            return -1;
-        }
-
+    static int binarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
-
             // find the middle element
             int mid = start + (end - start) / 2; // might be possible that (start + end) exceeds the range of int in java
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
+            if (target > arr[mid]) {
                 start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
             } else {
                 return mid;
             }
         }
-
-        return start;
+        return end;
     }
 }
