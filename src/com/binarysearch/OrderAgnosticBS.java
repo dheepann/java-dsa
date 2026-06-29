44215668566cb3b6443cf1aca13cc5b3fc6201bb
Dheepan <dheeepans09@gmail.com>
2026-06-29 20:30:13 +0530

    search in mountain array(leetcode problem)

--- src/com/binarysearch/OrderAgnosticBS.java
+++ src/com/binarysearch/OrderAgnosticBS.java
@@ -2,26 +2,40 @@ package com.binarysearch;
 
 public class OrderAgnosticBS {
     public static void main(String[] args) {
-        int[] arr = {100, 79, 66, 24, 6, 0, -3, -8};
-        int target = 66;
-        int ans = orderagnosticbs(arr, target);
+        int[] arr = {99, 80, 75, 22, 11, 10, 5, 2, -3};
+        int target = 22;
+        int ans = orderAgnosticBS(arr, target);
         System.out.println(ans);
     }
 
-    // return the index
-    // return -1 if it does not exist
-    static int orderagnosticbs(int[] arr, int target) {
+    static int orderAgnosticBS(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
+
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
         while (start <= end) {
             // find the middle element
-            int mid = start + (end - start) / 2; // might be possible that (start + end) exceeds the range of int in java
-            if (target > arr[mid]) {
-                end = mid - 1;
-            } else if (target < arr[mid]) {
-                start = mid + 1;
-            } else {
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
                 return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
         }
         return -1;
