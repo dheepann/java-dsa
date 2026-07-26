b2fab2b71e364249ec00c83f66d2622ade913192
Dheepan <dheeepans09@gmail.com>
2026-07-26 19:26:31 +0530

    find duplicate in array using cycle sort

--- src/com/sortingtechniques/FindDuplicate.java
+++ src/com/sortingtechniques/FindDuplicate.java
@@ -3,29 +3,28 @@ package com.sortingtechniques;
 
 public class FindDuplicate {
 
-    public class FindDuplicate {
-        public int findDuplicate(int[] arr) {
-            int i = 0;
-            while (i < arr.length) {
+    public int findDuplicate(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
 
-                if (arr[i] != i + 1) {
-                    int correct = arr[i] - 1;
-                    if (arr[i] != arr[correct]) {
-                        swap(arr, i, correct);
-                    } else {
-                        return arr[i];
-                    }
+            if (arr[i] != i + 1) {
+                int correct = arr[i] - 1;
+                if (arr[i] != arr[correct]) {
+                    swap(arr, i, correct);
                 } else {
-                    i++;
+                    return arr[i];
                 }
+            } else {
+                i++;
             }
-            return -1;
         }
-
-        static void swap(int[] arr, int first, int second) {
-            int temp = arr[first];
-            arr[first] = arr[second];
-            arr[second] = temp;
-        }
+        return -1;
     }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
 }
