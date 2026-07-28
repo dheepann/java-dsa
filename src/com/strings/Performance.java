26f029fc0c1d700fb6b0f9b34d86e9cea86a2dda
Dheepan <dheeepans09@gmail.com>
2026-07-28 19:52:09 +0530

    Problem with strings and how it's rectified

--- src/com/strings/Performance.java
+++ src/com/strings/Performance.java
@@ -1,10 +1,13 @@
 package com.strings;
+//important concept
 
 public class Performance {
     public static void main(String[] args) {
         String series = "";
         for (int i = 0; i < 26; i++) {
-            char ch = (char) ('a' + i);
+            char ch = (char) ('a' + i); //new str object is created everytime and so much memory wastage ie., a, ab,abc,abcd,etc will have no ref variable and wastage of space
+            // this has a time complexity of O(N^2). to solve these problems we have StringBuilder where we can directly insert into string w/o wasting memory like in arrays
+
 //            System.out.println(ch); //prints a to z
 
             series = series + ch;
