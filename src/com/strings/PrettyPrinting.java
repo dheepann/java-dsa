5481b6d33bd622bcf3dcfccbb1922cf44bc19dc0
Dheepan <dheeepans09@gmail.com>
2026-07-28 18:08:01 +0530

    Pretty Printing

---
+++ src/com/strings/PrettyPrinting.java
@@ -0,0 +1,13 @@
+package com.strings;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.12794f;
+//        System.out.printf("Formatted number is %.2f", a); //prints 2 decimal places also round off if possible
+//        System.out.println(Math.PI);
+//        System.out.printf("pie= %.2f", Math.PI);
+        System.out.printf("Hello my name is %s and Iam %s", "Dheepan", "cool");
+          
+
+    }
+}
