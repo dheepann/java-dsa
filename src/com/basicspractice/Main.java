4daa3371adc0778d302872f4119a84ebb7c7ef2c
Dheepan <dheeepans09@gmail.com>
2026-08-11 18:03:56 +0530

    basic java programs

--- src/com/basicspractice/Main.java
+++ src/com/basicspractice/Main.java
@@ -8,6 +8,14 @@ public class Main {
         System.out.println(maximum(99, 98));
         System.out.println(isPositive(-65));
         System.out.println(greet("Dheepan"));
+
+        int[] arr = {5, 12, 7, 20, 3, 7};
+        System.out.println(countGreaterThanTen(arr));
+        System.out.println(countEven(arr));
+        System.out.println(average(arr));
+
+        int target = 7;
+        System.out.println(countOccurences(arr, target));
     }
 
     public static int subtract(int a, int b) {
@@ -32,5 +40,46 @@ public class Main {
 
     public static String greet(String name) {
         return "Hello " + name;
+    }
+
+    public static int countGreaterThanTen(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 10) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countEven(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // find minimum is similar to find maximum
+    // q4) calculate average
+    public static double average(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        double avg = sum / arr.length;
+        return avg;
+    }
+
+    public static int countOccurences(int[] arr, int target) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                count++;
+            }
+        }
+        return count;
     }
 }
