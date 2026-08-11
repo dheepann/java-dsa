b63e5531702fc2809a5395f0e229f887e12357aa
Dheepan <dheeepans09@gmail.com>
2026-08-11 16:19:03 +0530

    basic java programs

---
+++ src/com/binarysearch/SmallestLetter.java
@@ -0,0 +1,28 @@
+package com.binarysearch;
+
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+public class SmallestLetter {
+    public static void main(String[] args) {
+
+    }
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+
+            // find the middle element
+            int mid = start + (end - start) / 2; // might be possible that (start + end) exceeds the range of int in java
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else if (target > letters[mid]) {
+                start = mid + 1;
+            }
+        }
+
+        return letters[start % letters.length];
+
+    }
+
+
+}
