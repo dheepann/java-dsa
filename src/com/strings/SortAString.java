fdbd7c3d729475e95eea849c17ad378b8ee8b3c3
Dheepan <dheeepans09@gmail.com>
2026-09-11 15:19:41 +0530

    sort a string

--- src/com/strings/SortAString.java
+++ src/com/strings/SortAString.java
@@ -6,12 +6,13 @@ public class SortAString {
     public static void main(String[] args) {
         String str = "cabfed";
 
-        //convert string to cahr array so that it's mutable
+        //convert string to char array so that it's mutable
         char[] chars = str.toCharArray();
 
         //sort the character array
         Arrays.sort(chars);
-
+        
+        // convert the character array to string in order to print as string
         String sortedString = new String(chars);
         System.out.println(sortedString);
     }
