cf5213b74ab30a7a907c47331b590c80022899a9
Dheepan <dheeepans09@gmail.com>
2026-09-11 14:39:57 +0530

    print a to z without stringbuilder

--- src/com/strings/Methods.java
+++ src/com/strings/Methods.java
@@ -6,8 +6,11 @@ public class Methods {
     public static void main(String[] args) {
         String name = "Dheepan hello world";
         System.out.println(name.toString());
-        System.out.println(name); // same as above
+        System.out.println(name); // same output as above
+
         System.out.println(Arrays.toString(name.toCharArray())); //check output
+        System.out.println(name.toCharArray());
+
         System.out.println(name.toLowerCase()); //prints dheepan hello world
         System.out.println(name.indexOf('a'));
 
--- src/com/strings/Performance.java
+++ src/com/strings/Performance.java
@@ -5,12 +5,12 @@ public class Performance {
     public static void main(String[] args) {
         String series = "";
         for (int i = 0; i < 26; i++) {
-            char ch = (char) ('a' + i); //new str object is created everytime and so much memory wastage ie., a, ab,abc,abcd,etc will have no ref variable and wastage of space
-            // this has a time complexity of O(N^2). to solve these problems we have StringBuilder where we can directly insert into string w/o wasting memory like in arrays
+            char ch = (char) ('a' + i); //new string object is created everytime and so much memory wasted ie., a, ab,abc,abcd,etc will have no ref variable and wastage of space
+            // this has a time complexity of O(N^2). to solve these problems we have StringBuilder where we can directly insert into string w/o craeting new object everytime like in arrays
 
 //            System.out.println(ch); //prints a to z
 
-            series = series + ch;
+            series = series + ch; // a, ab, abc, abcd and so on..
         }
         System.out.println(series);
     }
