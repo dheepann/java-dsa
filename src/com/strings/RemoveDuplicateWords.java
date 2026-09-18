162debe3b17abeb71d8eb4decc7a2deb4a81df51
Dheepan <dheeepans09@gmail.com>
2026-09-18 11:32:48 +0530

    remove duplicate words

--- src/com/strings/RemoveDuplicateWords.java
+++ src/com/strings/RemoveDuplicateWords.java
@@ -3,24 +3,20 @@ package com.strings;
 public class RemoveDuplicateWords {
     public static void main(String[] args) {
         String sentence = "Java spring java Angular spring java";
-        removeDuplicates(sentence);
+        removeDuplicates(sentence); // without using HashSet
     }
 
-    private static void removeDuplicates(String str) {
-        String sentence = "java spring java angular spring java";
-
-        String[] words = sentence.split(" ");
-
-        StringBuilder result = new StringBuilder();
+    static void removeDuplicates(String sentence) {
+        String[] words = sentence.split(" "); // first we create an array of string using .split() method
+        StringBuilder res = new StringBuilder(); // we add in the result sb only after removing duplicates using below logic
 
         for (String word : words) {
-
             boolean duplicate = false;
 
-            String[] resultWords = result.toString().split(" ");
+            String[] resultWords = res.toString().split(" "); // we convert the res sb to string array resultWords in order to compare with the original String[] words.
+            //  if a word present in original string words is already in res we declare duplicate is present and we break and we contiue the loop to the next word in the string array
 
             for (String existingWord : resultWords) {
-
                 if (existingWord.equalsIgnoreCase(word)) {
                     duplicate = true;
                     break;
@@ -28,16 +24,11 @@ public class RemoveDuplicateWords {
             }
 
             if (!duplicate) {
-
-                if (result.length() > 0) {
-                    result.append(" ");
-                }
-
-                result.append(word);
+                if (sentence.length() > 0) res.append(" "); // if a word is already added i add space for the next word
+                res.append(word);
             }
+
         }
-
-        System.out.println(result);
-
+        System.out.println(res);
     }
 }
